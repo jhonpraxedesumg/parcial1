cdb0fadb61b75a3bf0b8b2546908cd68107d649d
Praxe <jsalazarz@miumg.edu.gt>
2023-08-04 21:32:58 -0600

    parcial

--- src/main/java/com/example/fxejemplo/VentanasControles.java
+++ src/main/java/com/example/fxejemplo/VentanasControles.java
@@ -4,11 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class VentanasControles {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("formulario examen parcial");
+
     }
 }
